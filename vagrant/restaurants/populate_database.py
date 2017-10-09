340c8e5351bcdaa44bdf835eed279d4dbc41169d
Thomas Fresneau <thom.fresneau@gmail.com>
2017-10-09 14:23:31 +0200

    Populate database in Python

---
+++ vagrant/restaurants/populate_database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+session.commit()
+
+cheezePizza = MenuItem(name = "Margarita", description = "La famosa Margarita", course = "Entree", price = "8.99€", Restaurant = myFirstRestaurant)
+session.add(cheezePizza)
+session.commit()
