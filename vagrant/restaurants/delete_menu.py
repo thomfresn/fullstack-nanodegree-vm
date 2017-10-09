d97ebf508ad4b775bc5c3efe8828ce642abec9a6
Thomas Fresneau <thom.fresneau@gmail.com>
2017-10-09 14:41:10 +0200

    Delete menu

---
+++ vagrant/restaurants/delete_menu.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+spinach = session.query(MenuItem).filter_by(name = 'Spinach Ice Cream').one()
+print(spinach.restaurant.name)
+
+session.delete(spinach)
+session.commit()
