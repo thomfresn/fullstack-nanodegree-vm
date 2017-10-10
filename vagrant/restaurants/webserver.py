2dbbcb40adcf06d333ab612f1af9f536645a886d
Thomas Fresneau <thom.fresneau@gmail.com>
2017-10-10 10:14:56 +0200

    Add form to register a new restaurant

--- vagrant/restaurants/webserver.py
+++ vagrant/restaurants/webserver.py
@@ -16,19 +16,6 @@ import cgi
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html', )
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output.encode("utf-8"))
-                print(output)
-                return
-
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html', )
@@ -47,6 +34,19 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print(output)
                 return
 
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html', )
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Enter a new restaurant!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>New restaurant name</h2><input name="restaurantName" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output.encode("utf-8"))
+                print(output)
+                return
+
         except IOError:
             self.send_error(404, "File not found %s" % self.path)
 
@@ -59,15 +59,18 @@ class webserverHandler(BaseHTTPRequestHandler):
             if ctype == 'multipart/form-data':
                 pdict['boundary'] = bytes(pdict['boundary'], 'utf-8')
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')[0].decode('utf-8')
-            output = ''
-            output += '<html><body>'
-            output += '<h2> Okay, how about this: </h2>'
-            output += '<h1> %s </h1>' % messagecontent
-            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
-            output += '</html></body>'
-            self.wfile.write(output.encode('utf-8'))
-            print(output)
+                restaurantName = fields.get('restaurantName')[0].decode('utf-8')
+                newRestaurant = Restaurant(name = restaurantName)
+                session.add(newRestaurant)
+                session.commit()
+
+                output = ''
+                output += '<html><body>'
+                output += '<h2> New restaurant created: </h2>'
+                output += '<h1> %s </h1>' % restaurantName
+                output += '</html></body>'
+                self.wfile.write(output.encode('utf-8'))
+                print(output)
         except:
                 pass
 
