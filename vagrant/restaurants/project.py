90256cda897447661b8662b9ff883b137597b781
Thomas Fresneau <thom.fresneau@gmail.com>
2017-10-11 09:37:40 +0200

    Display messages using Flakh flash messages when items are created edited and deleted

--- vagrant/restaurants/project.py
+++ vagrant/restaurants/project.py
@@ -1,5 +1,5 @@
 #import Flask module
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 #import SQLAlchemy module
 from sqlalchemy import create_engine
@@ -35,32 +35,17 @@ def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
     output = ''
-    for menu in items:
-        output += "<div>"
-        output += menu.name
-        output += "<br>"
-        output += "<a href='/restaurants/%s/%s/edit'>" % (restaurant.id, menu.id)
-        output += "Edit menu"
-        output += "</a>"
-        output += "<br>"
-        output += "<a href='/restaurants/%s/%s/delete'>" % (restaurant.id, menu.id)
-        output += "Delete menu"
-        output += "</a>"
-        output += "<br>"
-        output += "</div>"
-        output += "</body></html>"
-
-    return output
+    return render_template('menu.html', restaurant=restaurant, items = items)
 
 
 @app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-
     if request.method == 'POST':
         newItem = MenuItem(name=request.form['name'], description=request.form[
                            'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash('New item created')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -74,6 +59,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash('item edited')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
@@ -85,10 +71,12 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
+        flash('item deleted')
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=deletedItem)
 
 if __name__ == '__main__':
+    app.secret_key = "super_secret_key"
     app.debug = True
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
